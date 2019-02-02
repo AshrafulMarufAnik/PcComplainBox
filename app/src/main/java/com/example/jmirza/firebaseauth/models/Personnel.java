771d636efb3ca0eaca989b83f8a00f6185ad809f
jmirja <41453618+jmirja@users.noreply.github.com>
2019-02-03 03:18:33 +0600

    worked on notification for all version

--- app/src/main/java/com/example/jmirza/firebaseauth/models/Personnel.java
+++ app/src/main/java/com/example/jmirza/firebaseauth/models/Personnel.java
@@ -7,24 +7,7 @@ public class Personnel {
     public Personnel(){
 
     }
-    public Personnel(String name, String gender, String phone, String email, String password, String occupation) {
-        this.occupation=occupation;
-        this.name = name;
-        this.gender = gender;
-        this.phone = phone;
-        this.email = email;
-        this.password=password;
 
-    }
-    public Personnel(String name, String gender, String phone, String email, String password, String occupation,String deviceToken) {
-        this.occupation=occupation;
-        this.name = name;
-        this.gender = gender;
-        this.phone = phone;
-        this.email = email;
-        this.password=password;
-        this.deviceToken=deviceToken;
-    }
 
     public Personnel(String key,String name, String gender, String phone, String email, String password, String occupation,String deviceToken) {
         this.key = key;
--- app/src/main/java/com/example/jmirza/firebaseauth/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/jmirza/firebaseauth/services/MyFirebaseMessagingService.java
@@ -1,12 +1,15 @@
 package com.example.jmirza.firebaseauth.services;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.example.jmirza.firebaseauth.R;
@@ -17,6 +20,23 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FirebaseMessagingServce";
+    private static final String CHANNEL_ID = "Notification_channel";
+    private static final String CHANNEL_NAME = "Notification_name";
+    private static final String CHANNEL_DES = "Notification_description";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DES);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -38,23 +58,23 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String notificationTitle, String notificationBody) {
+
         Intent intent = new Intent(this, ComplainFragment.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =  new NotificationCompat.Builder(this)
+        NotificationCompat.Builder notificationBuilder =  new NotificationCompat.Builder(this,CHANNEL_ID)
                 .setAutoCancel(true)   //Automatically delete the notification
-                .setSmallIcon(R.drawable.varsity_logo) //Notification icon
+                .setSmallIcon(R.drawable.varsity_logo)
                 .setContentIntent(pendingIntent)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationBody)
                 .setSound(defaultSoundUri);
 
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(0,notificationBuilder.build());
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0, notificationBuilder.build());
     }
 }
